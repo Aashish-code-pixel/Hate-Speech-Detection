ffd9d6542575cd754bb564bc368ec350d6cd3aeb
Aashish-code-pixel <144586455+Aashish-code-pixel@users.noreply.github.com>
2025-02-14 12:48:33 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,15 @@
+import joblib
 
+# Load saved model and vectorizer
+model = joblib.load("models/hate_speech_model.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
+
+# Function to predict text
+def predict_hate_speech(text):
+    text_tfidf = vectorizer.transform([text])
+    prediction = model.predict(text_tfidf)
+    return "Hate Speech" if prediction == 1 else "Not Hate Speech"
+
+# Test prediction
+text = input("Enter text: ")
+print("Prediction:", predict_hate_speech(text))
